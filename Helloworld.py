f08642135f3211405d81b16e3fc51473b950fb58
gauravmtwt1 <ecegauravmathur90@gmail.com>
2023-08-03 11:00:55 +0530

    Helloworld

---
+++ Helloworld.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print("Aapka Swaagat hain")
+
+name =  "Gaurav Mathur"
+age = 33
+price = 0.0
+print(type(name))
+print(type(age))
+print(type(price))
+enjoying = True
+print(type(enjoying))
+
+print("My name is " +name)
+print("My name is ", name)
