f7d1bc325ca8cdd27f30d392f1d31f878ebec80f
gauravmtwt1 <ecegauravmathur90@gmail.com>
2024-08-02 07:54:10 +0530

    Useful_Linux_Utilities.py

---
+++ Useful_Linux_Utilities.py
@@ -0,0 +1,45 @@
+# Useful Linux utilities
+
+# 1. Command Line interface(CLI) Tools:
+# 'bash': The Bourne Again Shell, a command language interpreter
+# 'ssh': Secure Shell, used for securely accessing remote servers.
+# 'scp' and 'rsync': Tools for copying files over SSH and syncing files/directories.
+# 'screen' and 'tmux' : Terminal multiplexers for managing multiple session.
+
+# 2. System Monitoring Tools
+# 'top' and 'htop': Real-time system monitoring tools to see processes and resource usage.
+# 'iostat': Monitors system input/output device loading.
+# 'vmstat': Reports information about processes, memory, paging, block IO, traps, and CPU
+#           activity.
+# 'netstat': Display network connections, routing tables, interface statistics, masquerade,
+#            connections, and multicast memberships.
+#"lsof": Lists open files and the processes that opened them.
+
+# 3. File and Text Processing Utilities
+# 'grep': Searches for patterns in files.
+# 'awk': A programming language for pattern scanning and processing.
+# 'sed': Stream editor for filtering and transforming text.
+# 'cut' , 'sort', 'uniq': Utilities for Manipulating text data.
+
+# 4. Package Management Tools
+# 'apt-get' / 'apt': Advanced Package Tool for Debian-based systems.
+# 'yum'/'dnf': Package managers for RPM-based distributions.
+
+# 5. Version Controls Systems
+# Subversion(SVN): A centralized version control system.
+# Git: A distributed version control system widely used in modern development.
+
+# 6. Network Utilities:
+# 'ping': Checks connectivity to other network devices.
+# 'traceroute': Shows the path packets take to reach a network host.
+# 'curl' and 'wget': Tools for transferring data from or to a server.
+
+# 7. Authentication and Security Tools:
+# 'openssl': Toolkit for SSL/TLS and cryptographic operations.
+# 'fail2ban': Scans log files and bans IPs that show malicious signs.
+# 'iptables': Administration tool for IPV4 packet filtering and NAT.
+
+# 8. Automation and Scripting:
+# Shell Scripting: Writing scripts to automate tasks.
+# 'cron': Scheduling jobs to run at specific times.
+# 'systemd'/'init': Managing Services and Boot-time processes.
