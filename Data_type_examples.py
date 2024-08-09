2f9154836e76c412b55cab48c5c188925cde5276
gauravmtwt1 <ecegauravmathur90@gmail.com>
2024-08-09 09:47:20 +0530

    Data_type_examples.py

---
+++ Data_type_examples.py
@@ -0,0 +1,24 @@
+
+# 1. String Concat
+
+str1 = "Hello"
+str2 = "World"
+result = str1 + " " + str2
+print(result)
+
+# 2. String-len
+text = "Python is awesome"
+length = len(text)
+print("Lenght of the text: ", text)
+
+# 3. String-lowercase
+text1 = "Python is awesome"
+uppercase = text.upper()
+lowercase = text.lower()
+print("Uppercase: ", uppercase)
+print("Lowercase: ", lowercase)
+
+# 4. String replace
+text2 = "Python is awesome"
+new_text = text.replace("awesome","great")
+print("Modified text: ", new_text)
